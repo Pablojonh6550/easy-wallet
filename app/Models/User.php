0702f9523b87c397a333b96dc6cab39e04d446d0
Pablojonh6550 <pablojonh6550@gmail.com>
2025-05-01 19:41:00 -0300

    refactor: adição função para criptografia de senha

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,11 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
         ];
+    }
+
+    public function setPasswordAttribute($password): void
+    {
+        $this->attributes['password'] = bcrypt($password);
     }
 }
