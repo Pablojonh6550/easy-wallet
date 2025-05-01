9791037397ee40a4c27a169bda5186e5eaff00c7
Pablojonh6550 <pablojonh6550@gmail.com>
2025-05-01 19:42:43 -0300

    chore: resgitro das interfaces de usuário

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,16 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+// Interfaces
+use App\Interfaces\BaseInterface;
+use App\Interfaces\User\UserInterface;
+// Repositories
+use App\Repositories\BaseRepository;
+use App\Repositories\User\UserRepository;
+
+use App\Interfaces\Auth\AuthInterface;
+use App\Services\Auth\AuthService;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,7 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(BaseInterface::class, BaseRepository::class);
+        $this->app->bind(AuthInterface::class, AuthService::class);
+        $this->app->bind(UserInterface::class, UserRepository::class);
     }
 
     /**
