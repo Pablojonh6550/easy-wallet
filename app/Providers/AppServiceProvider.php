729b8b0a5e2ceb5d602207ebeea297c4426b993a
Pablojonh6550 <pablojonh6550@gmail.com>
2025-05-02 13:21:17 -0300

    chore: registro das interfaces de databank e transaction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,9 +7,13 @@ use Illuminate\Support\ServiceProvider;
 // Interfaces
 use App\Interfaces\BaseInterface;
 use App\Interfaces\User\UserInterface;
+use App\Interfaces\DataBank\DataBankInterface;
+use App\Interfaces\Transaction\TransactionInterface;
 // Repositories
 use App\Repositories\BaseRepository;
 use App\Repositories\User\UserRepository;
+use App\Repositories\Transaction\TransactionRepository;
+use App\Repositories\DataBank\DataBankRepository;
 
 use App\Interfaces\Auth\AuthInterface;
 use App\Services\Auth\AuthService;
@@ -24,6 +28,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(BaseInterface::class, BaseRepository::class);
         $this->app->bind(AuthInterface::class, AuthService::class);
         $this->app->bind(UserInterface::class, UserRepository::class);
+        $this->app->bind(TransactionInterface::class, TransactionRepository::class);
+        $this->app->bind(DataBankInterface::class, DataBankRepository::class);
     }
 
     /**
