d3ad75c0d446ffcac8311aa604b3872795ee171d
Pablojonh6550 <pablojonh6550@gmail.com>
2025-05-04 18:31:44 -0300

    refactor: adição HasFactory

--- app/Models/DataBank.php
+++ app/Models/DataBank.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DataBank extends Model
 {
+    use HasFactory;
+
     protected $table = 'data_banks';
 
     protected $fillable = [
