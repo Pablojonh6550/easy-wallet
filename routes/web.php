f252d0e8927cd170a7f5de4e1c4b55c5de5df816
Pablojonh6550 <pablojonh6550@gmail.com>
2025-05-04 11:18:29 -0300

    refactor: ajuste nos registro de rota e permissões

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Action\Deposit\DepositController;
+use App\Http\Controllers\User\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/', [AuthController::class, 'showLogin'])->name('login');
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/', [AuthController::class, 'login'])->name('form-login');
+    Route::post('/register', [AuthController::class, 'register'])->name('form-register');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::get('/teste', function () {
-    return 'Funcionando!';
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
+    Route::get('/deposit', [DepositController::class, 'showDeposit'])->name('deposit');
 });
