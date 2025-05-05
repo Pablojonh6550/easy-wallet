e68d5eb382f252d098929af41aabdee9bb676b59
Pablojonh6550 <pablojonh6550@gmail.com>
2025-05-04 23:34:01 -0300

    feat: adiçao dos relacionamentos das  transações com  o usuário

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\DataBank;
 
 class Transaction extends Model
 {
@@ -17,4 +20,19 @@ class Transaction extends Model
         'type',
         'user_id_receiver',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function dataBank(): BelongsTo
+    {
+        return $this->belongsTo(DataBank::class, 'data_bank_id');
+    }
+
+    public function receiver(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id_receiver');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Authenticatable
 {
@@ -50,8 +52,13 @@ class User extends Authenticatable
         $this->attributes['password'] = bcrypt($password);
     }
 
-    public function dataBank()
+    public function dataBank(): HasOne
     {
         return $this->hasOne(DataBank::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
     }
 }
